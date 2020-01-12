acc22730c749e4d39c459b5e75be136a98d55a4e
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-12 19:10:21 +0100

    app.js hello test

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({
+   "model": {
+        "frames": [
+            {
+               "icon":2867,
+               "text":"Hello!"
+            }
+        ]
+    }
+  })
 })
 
 app.listen(3000, function () {
